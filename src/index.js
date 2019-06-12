7bdd10f21ecd357900082c5c84dc990bb4f888fa
Chris <chris@uber5.com>
2019-06-12 15:49:32 +0200

    Supposedly fixed missing export of ButtonSet component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// TODO: HelloWorld serves as example of how to do samples in the docs, should be removed later
 export { default as HelloWorld } from './components/hello-world'
+
+export { default as ButtonSet } from './components/ButtonSet'
